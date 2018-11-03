96b0554dafb8e2bbb4cda9ac3e145c7eeb615bd4
Sascha <info@aviator-coding.de>
2018-11-03 10:44:03 -0400

    Alpine Friendly

--- script/bulwarkd_setup.sh
+++ script/bulwarkd_setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # Download latest node and install.
 bwklink=`curl -s https://api.github.com/repos/bulwark-crypto/bulwark/releases/latest | grep browser_download_url | grep linux64 | cut -d '"' -f 4`
 mkdir -p /tmp/bulwark
